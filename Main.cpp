ab90b8ed4b7bd2c7f218401ce14ad2629fff7164
do12mink123 <dominik.szczypior01@gmail.com>
2018-04-28 20:26:40 +0200

    Add files via upload

---
+++ Main.cpp
@@ -0,0 +1,191 @@
+
+#include "Main.h"
+
+#define ilstr 14
+
+const char *smeme[ilstr] = {
+	"http://mamtak.pl/page/",
+	"http://takmam.pl/page/",
+	"https://besty.pl/pages/",
+	"https://fabrykamemow.pl/page/",
+	"https://jbzdy.pl/strona/",
+	"https://www.chamsko.pl/page/",
+	"https://demotywatory.pl/page/",
+	"http://komixxy.pl/page/",
+	"https://pocisk.org/page/",
+	"https://mistrzowie.org/page/",
+	"http://redmik.pl/",
+	"http://memy.pl/?page=",
+	"http://www.hopaj.pl/hopy,",
+	"https://kwejk.pl/strona/",
+};
+
+std::string smemeo[ilstr] = {
+	"http://mamtak.pl",
+	"http://takmam.pl",
+	"https://besty.pl",
+	"https://fabrykamemow.pl",
+	"https://jbzdy.pl",
+	"https://www.chamsko.pl",
+	"https://demotywatory.pl",
+	"http://komixxy.pl",
+	"https://pocisk.org",
+	"https://mistrzowie.org",
+	"http://redmik.pl",
+	"http://memy.pl",
+	"http://www.hopaj.pl",
+	"https://kwejk.pl",
+};
+
+
+int main() {
+	using namespace std;
+
+	for (int i=0; i < ilstr; i++) {
+		int st = 1;
+		int bl = 0;
+		bool dzielaj = 1;
+		string phstr;
+		while (dzielaj) {
+			string aaa = smeme[i];
+			aaa += to_string(st);
+			int timo = 0;
+		ponowww:
+			HRESULT hr = URLDownloadToFile(
+				NULL,
+				aaa.data(),
+				"temp.html",
+				0,
+				NULL);
+			timo++;
+			if ((hr < 0) && (timo < 5))goto ponowww;
+			if (SUCCEEDED(hr))
+			{
+				string nh = gh("temp.html");
+				if (nh == phstr) {
+					dzielaj = 0;
+					break;
+				}
+				phstr = nh;
+				fstream ttt("temp.html", ios::in | ios::binary);
+				if (ttt.good())
+					while (!ttt.eof()) {
+						char z = 0;
+						ttt.read(&z, 1);
+						if (z != '.') {
+							continue;
+						}
+						ttt.read(&z, 1);
+						bool gooo = 0;
+						string konc;
+						switch (z)
+						{
+						case('j')://jpg
+						{
+							ttt.read(&z, 1);
+							if (z == 'p') {
+								ttt.read(&z, 1);
+								if (z == 'g') {
+									gooo = 1;
+									konc = ".jpg";
+								}
+							}
+						}
+						break;
+						case('p')://png
+						{
+							ttt.read(&z, 1);
+							if (z == 'n') {
+								ttt.read(&z, 1);
+								if (z == 'g') {
+									gooo = 1;
+									konc = ".png";
+								}
+							}
+						}
+						break;
+						case('b')://bmp
+						{
+							ttt.read(&z, 1);
+							if (z == 'm') {
+								ttt.read(&z, 1);
+								if (z == 'p') {
+									gooo = 1;
+									konc = ".bmp";
+								}
+							}
+						}
+						break;
+						case('g')://bmp
+						{
+							ttt.read(&z, 1);
+							if (z == 'i') {
+								ttt.read(&z, 1);
+								if (z == 'f') {
+									gooo = 1;
+									konc = ".gif";
+								}
+							}
+						}
+						break;
+						case('h'):
+						{
+							ttt.read(&z, 1);
+							if (z == 't') {
+								ttt.read(&z, 1);
+								if (z == 'm') {
+									ttt.read(&z, 1);
+									if (z == 'l') {
+										gooo = 0;//lol
+										konc = ".html";
+									}
+								}
+							}
+						}
+						break;
+						default:
+							break;
+						}
+
+						if (gooo == 1) {
+							int d = ttt.tellp();
+							while (1) {
+								d--;
+								ttt.seekp(d);
+								ttt.read(&z, 1);
+								if (z == '"')break;
+							}
+							string a;
+							while (1) {
+								ttt.read(&z, 1);
+								if (z == '"')break;
+								a += z;
+							}
+							if ((a.data()[0] == '/') || (a.data()[0] == '\\')) {
+								pkol(smemeo[i] + a, konc);
+							}
+							else {
+								pkol(a, konc);
+							}
+						}
+					}
+				ttt.close();
+#ifndef NC
+				DeleteFile("temp.html");
+#endif // !NC
+
+			}
+			else {
+				if (bl > 10)dzielaj = 0;
+				bl++;
+			}
+			st++;
+		}
+
+	}
+	return 0;
+}
+
+
+
+
---
+++ Main.h
@@ -0,0 +1,146 @@
+#pragma once
+
+#include "openssl/sha.h"
+#include <openssl/ssl.h>
+#include <openssl/ec.h>
+#include <openssl/ripemd.h>
+#include <fstream>
+#include <math.h>
+#include <iostream>
+#include <string>
+
+#ifdef _WIN32
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include <ws2def.h>
+
+#pragma comment(lib,"ws2_32.lib")
+#pragma comment(lib,"libcrypto.lib")
+#pragma comment(lib,"urlmon.lib")
+
+namespace suuuuu {
+	struct SocketInitializer
+	{
+		SocketInitializer() {
+			WSAData init;
+			WSAStartup(MAKEWORD(2, 2), &init);
+
+
+		}
+		~SocketInitializer() {
+			WSACleanup();
+		}
+	};
+	SocketInitializer glo;
+};
+#else
+
+#include <errno.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netinet/tcp.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <unistd.h>
+
+#endif
+
+void base58_encode(unsigned char *data, unsigned int len, char *result) {
+	const char code_string[] = "123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz";
+
+	BIGNUM *x, *rem, *base, *tmp, *tmp2;
+	x = BN_new();
+	rem = BN_new();
+	base = BN_new();
+	tmp = BN_new();
+	char * output_string = (char *)malloc(64);
+
+	x = BN_bin2bn(data, len, x);
+
+	BN_set_word(rem, 1);
+	BN_set_word(base, 58);
+
+	BN_CTX *bnctx;
+	bnctx = BN_CTX_new();
+
+	int i = 0;
+	while (!BN_is_zero(x)) {
+		BN_div(tmp, rem, x, base, bnctx);
+		output_string[i++] = code_string[BN_get_word(rem)];
+
+
+		tmp2 = x;
+		x = tmp;
+		tmp = tmp2;
+	}
+
+
+	int i2 = 0;
+	while (data[i2] == 0) {
+		output_string[i++] = code_string[0];
+		i2++;
+	}
+
+	int base58len = i;
+	while (i >= 0) {
+		result[base58len - i] = output_string[i - 1];
+		i--;
+	}
+	result[base58len] = 0;
+
+	BN_free(x);
+	BN_free(base);
+	BN_free(rem);
+	BN_free(tmp);
+	BN_CTX_free(bnctx);
+	free(output_string);
+}
+
+std::string gh(std::string nzw) {
+	using namespace std;
+	fstream plik(nzw, ios::in | ios::binary);
+	int dl = 0;
+	plik.seekg(0, ios::end);
+	dl = plik.tellg();
+	plik.seekg(0);
+	if (dl > 1000000000)return "1";
+	if (dl <= 0)return "0";
+	char *pl = new char[dl];
+	plik.read(pl, dl);
+	unsigned char md[SHA256_DIGEST_LENGTH];
+	unsigned char * haa = SHA256((unsigned char*)pl, dl, md);
+	delete[] pl;
+	char b58[64];
+	base58_encode(haa, SHA256_DIGEST_LENGTH, b58);
+	string w = b58;
+	return w;
+}
+
+void zobr(std::string tempz, std::string konc) {
+	using namespace std;
+	string hash = gh(tempz);
+	hash += konc;
+	fstream plik(hash, ios::in | ios::binary);
+	if (!plik.good()) {
+		if (CopyFile(tempz.data(), (string(" C:\\DATA\\") + hash).data(),0))
+		{
+			cout << hash << endl;
+		}
+	}
+	DeleteFile(tempz.data());
+}
+
+void pkol(std::string adr, std::string konc) {
+	HRESULT hr = URLDownloadToFile(
+		NULL,
+		adr.data(),
+		(std::string("temp") + konc).data(),
+		0,
+		NULL);
+	if (SUCCEEDED(hr))
+	{
+		zobr(std::string("temp") + konc, konc);
+	}
+}
